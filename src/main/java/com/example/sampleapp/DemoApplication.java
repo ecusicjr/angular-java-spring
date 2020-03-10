2463012c9cba2af03864702efecf28d14b814d30
Eamon Cusic Jr <ecusicjr@Eamons-MBP.fios-router.home>
2020-03-10 00:04:58 -0400

    Simple REST API

--- src/main/java/com/example/sampleapp/DemoApplication.java
+++ src/main/java/com/example/sampleapp/DemoApplication.java
@@ -2,18 +2,11 @@ package com.example.sampleapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.stereotype.*;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@Controller
 @SpringBootApplication
+@EnableJpaAuditing
 public class DemoApplication {
-
-    @RequestMapping("/")
-    @ResponseBody
-    String home() {
-      return "Hello World!";
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
